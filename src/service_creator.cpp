b0a9bfd0d5c1d9924f805f79371217eb6702a389
Tydecent <Smart100430@163.com>
2026-03-09 20:15:15 +0800

    fix:修复service_file的构建方式，原有构建方式编译器无法理解

--- src/service_creator.cpp
+++ src/service_creator.cpp
@@ -6,20 +6,21 @@ std::string construct_service_file(std::vector<std::string> inquire_result) {
     std::string service_name = inquire_result[1];
     std::string service_description = inquire_result[2];
 
-    std::string service_file = "[Unit]\n" +
-                               "Description=" + service_description + "\n"
-                               "After=network.target\n"
-                               "\n"
-                               "[Service]\n"
-                               "Type=simple\n"
-                               "ExecStart=" + exec_path + "\n"
-                               "Restart=on-failure\n"
-                               "RestartSec=3\n"
-                               "User=root\n"
-                               "Group=root\n"
-                               "\n"
-                               "[Install]"
-                               "WantedBy=multi-user.target";
-
+    std::string service_file;
+    service_file += "[Unit]\n";
+    service_file += "Description=" + service_description + "\n";
+    service_file += "After=network.target\n";
+    service_file += "\n";
+    service_file += "[Service]\n";
+    service_file += "Type=simple\n";
+    service_file += "ExecStart=" + exec_path + "\n";
+    service_file += "Restart=on-failure\n";
+    service_file += "RestartSec=3\n";
+    service_file += "User=root\n";
+    service_file += "Group=root\n";
+    service_file += "\n";
+    service_file += "[Install]";
+    service_file += "WantedBy=multi-user.target";
+    
     return service_file;
 }
