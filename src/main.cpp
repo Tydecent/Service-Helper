d634a23df19788360924b2588dd3ec541103acfe
Tydecent <Smart100430@163.com>
2026-03-11 16:51:10 +0800

    feat:在构造服务文件前添加添加可执行权限的步骤。

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,12 @@ int main(int argc, char *argv[]) {
     // 询问信息
     std::vector<std::string> inquire_result = Inquire();
 
+    // 为可执行文件添加可执行权限
+    if (add_executable_to_path(inquire_result[0]) == false) {
+        std::cout << "添加可执行权限失败。" << std::endl;
+        return -3;
+    }
+
     // 构造服务文件
     std::string service_file = construct_service_file(inquire_result);
 
