52ce5c5f1d7248a461c7f38d6a443004a494b7e9
Tydecent <Smart100430@163.com>
2026-03-12 08:00:34 +0800

    feat: 增加了检查shebang和询问时检查文件是否存在的功能。

--- src/utils.h
+++ src/utils.h
@@ -16,3 +16,7 @@ bool write_file(std::string, std::string);
 int system_call(std::string command);
 
 bool add_executable_to_path(std::string exec_path);
+
+bool check_shebang(std::string exec_path);
+
+bool check_file_exists(std::string exec_path);
