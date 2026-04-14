67398d5591747fdf254a4d09e473c13e74347486
Tydecent <Smart100430@163.com>
2026-04-14 14:27:23 +0800

    refactor: [main, utils]<add_service>将服务添加逻辑迁移到 `add_service` 函数
    将 `main` 函数中添加服务的逻辑抽取到一个新的函数 `add_service` 中，以提高代码的可读性和可维护性。
    
    - 在 `main` 函数中调用 `add_service` 函数来处理服务的添加。
    - 在 `utils.cpp` 文件中实现 `add_service` 函数，该函数包含了原有的服务添加逻辑。
    - 在 `utils.h` 文件中添加 `add_service` 函数的声明，以便在其他地方调用该函数。
    
    通过这种方式，将具体的业务逻辑从 `main` 函数中分离出来，使代码结构更加清晰。

--- src/utils.cpp
+++ src/utils.cpp
@@ -279,3 +279,36 @@ void print_version_info() {
               << "Git Hash: " << GIT_HASH << "\n"
               << "Build Time: " << BUILD_TIMESTAMP << std::endl;
 }
+
+int add_service() {
+        // 检查root权限
+    if (is_root() == false) {
+        std::cout << "添加服务需要root权限，请以root或sudo权限运行本程序。" << std::endl;
+        return -1;
+    }
+
+    // 询问信息
+    std::vector<std::string> inquire_result = Inquire();
+
+    // 处理相对路径
+    inquire_result[0] = get_absolute_path(inquire_result[0]);
+
+    // 检查可执行文件
+    check_exec(inquire_result[0]);
+
+    // 构造服务文件
+    std::string service_file = construct_service_file(inquire_result);
+
+    // 写入服务文件
+    if (write_file(service_file, "/etc/systemd/system/" + inquire_result[1] + ".service")) {
+        std::cout << "写入成功。" << std::endl;
+    } else {
+        std::cout << "文件写入失败。" << std::endl;
+        return -2;
+    }
+
+    // 使服务生效
+    effective_service(inquire_result[1] + ".service");
+
+    return 0;
+}
